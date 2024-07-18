abe046aac834dbcc26a044a40567f6017cce9d30
Diego Poupiot <poupiot.diego@gmail.com>
2024-07-18 14:37:53 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+let title = 'Hello Vite + Vue 3'
+
+function reverseTitle() {
+  title = title.split('').reverse().join('')
+  console.log('Title reversed:', title)
+}
+</script>
+
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <button v-on:click="reverseTitle">Reverse title</button>
+  </main>
+</template>
