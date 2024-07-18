55cc9b27c1016be6646fd49f3e1aa01ccad0bcab
Diego Poupiot <poupiot.diego@gmail.com>
2024-07-18 15:50:29 +0200

    properties computed & watcher

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,36 @@
 <script setup>
-import { ref } from 'vue'
+import {watch, computed, ref} from 'vue'
 
 let count = ref(0)
-let tagname = ref('')
+let count2 = ref(0)
+let word = ref("Positif")
 
-function handleKeyup(event) {
-  if (event.code === 'Space') {
-    alert('une touche')
-  }
+function wordMethod() {
+  console.log("In method.")
+  return count.value < 0 ? "Negatif" : "Positif"
 }
 
-function increase(step, e) {
-  count.value += step
-  tagname.value = e.target.tagName
-}
+// permet de créer une variable dépendante d'une autre variable
+const wordComputed = computed(() => {
+  console.log("In computed.")
+  return count.value < 0 ? "Negatif" : "Positif"
+})
 
-function hover(event) {
-  tagname.value = event.target.tagName
-}
+// watcher : dans le cas ou j'ai besoin d'un traitement asynchrone dans les computed properties
+const counter = watch(count, (newValue, oldValue) => {
+  word.value = count.value < 0 ? "Negatif" : "Positif"
+  console.log(`Count changed from ${oldValue} to ${newValue}.`)
+})
 
-function stopHover(e) {
-  e.stopPropagation()
-}
 </script>
 
 <template>
   <main>
+    <button @click="count++">Increase</button>
+    <button @click="count--">Decrease</button>
+    <button @click="count2++">Increase 2</button>
     <h1>{{ count }}</h1>
-    <button @click="increase(4, $event)">Increase</button>
-    <h2 @mouseover="hover">test</h2>
-    <h3 @mouseover.stop="">test</h3>
-    <p @mouseover="hover">
-      lorem ipsum dolor sit amet <br/>
-      Balise : {{ tagname }}
-    </p>
-    <input type="text" @keyup="handleKeyup">
+    <h4>{{ wordMethod() }} - {{ wordComputed }} - {{ word }}</h4>
+    <h1>{{ count2 }}</h1>
   </main>
 </template>
