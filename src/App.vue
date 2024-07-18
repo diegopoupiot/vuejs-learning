37e32bfc4ba19ca879dc9d0b94de91ba996c94c2
Diego Poupiot <poupiot.diego@gmail.com>
2024-07-18 15:04:29 +0200

    learning basics

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <script setup>
-let title = 'Hello Vite + Vue 3'
+import { ref } from 'vue'
+
+let title = ref('Hello Vite + Vue 3')
+let strongMessage = "<i>IMPORTANT</i>"
 
 function reverseTitle() {
-  title = title.split('').reverse().join('')
-  console.log('Title reversed:', title)
+  title.value = title.value.split('').reverse().join('')
+  console.log('Title reversed:', title.value)
 }
 </script>
 
 <template>
   <main>
+    <h1 v-once>{{ title }}</h1>
     <h1>{{ title }}</h1>
-    <button v-on:click="reverseTitle">Reverse title</button>
+    <h3>{{ title === 'Hello Vite + Vue 3' ? "ok" : "non ok" }}</h3>
+    <input type="text" v-model="title">
+    <button @click="reverseTitle">Reverse title</button>
+    <p v-once :title="title">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor
+      <span>
+        {{ title }}
+      </span>
+    </p>
+    <p v-html="strongMessage"></p>
   </main>
 </template>
